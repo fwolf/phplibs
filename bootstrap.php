f02da5090523d02c97f90718d3a4a52557f91505
Fwolf <fwolf.aide+git@gmail.com>
2015-03-12 01:54:58 +0800

    Change comment about how libs load shared vendor packages

--- bootstrap.php
+++ bootstrap.php
@@ -5,8 +5,10 @@
  *  - Register ClassLoader
  *  - Load default and user config
  *
- * Can be used by all libs, example phpunit config:
- *      <phpunit bootstrap='../../bootstrap.php' />
+ * This is only a dummy file, libs still use their own phpunit configure and
+ * bootstrap. But libs have no actual vendor/ downloaded, so need to put a dummy
+ * 'vendor/autoload.php' in each lib root dir, and  require real autoload file
+ * in far parent directory, which is actual composer downloaded packages in.
  *
  * @copyright   Copyright 2015 Fwolf
  * @license     http://opensource.org/licenses/MIT MIT
